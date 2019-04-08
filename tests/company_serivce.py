654ad427624283f5bbfe82311b57df724e72090c
dhkang <dhkang@nm-monster.com>
2019-04-08 15:51:20 +0900

    [API] #2 태그 검색 유닛 테스트
    태그 검색 API 유닛 테스트 코드 추가

--- tests/company_serivce.py
+++ tests/company_serivce.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 import unittest
-from app import app
+from app import create_app
 import json
 from http import HTTPStatus
+import itertools
 
 
 class CompanyServiceTest(unittest.TestCase):
 
     def setUp(self):
-        self.app = app.test_client()
+        self.app = create_app().test_client()
 
     def test_autocomplete(self):
+        print("===========================================")
         response = self.app.get('/company/autocomplete?input=W')
         self.assertEqual(response.status_code, HTTPStatus.OK)
         print("autocomplete : W")
@@ -45,6 +47,23 @@ class CompanyServiceTest(unittest.TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         print("autocomplete : ket")
         print(json.loads(response.get_data().decode()))
+
+    def test_tag(self):
+        print("===========================================")
+        response1 = self.app.get('/company/tag?input=태그_3')
+        self.assertEqual(response1.status_code, HTTPStatus.OK)
+        print("tag : 태그_3")
+        print(json.loads(response1.get_data().decode()))
+
+        response2 = self.app.get('/company/tag?input=タグ_3')
+        self.assertEqual(response2.status_code, HTTPStatus.OK)
+        print("tag : タグ_3")
+        print(json.loads(response2.get_data().decode()))
+
+        response3 = self.app.get('/company/tag?input=tag_3')
+        self.assertEqual(response3.status_code, HTTPStatus.OK)
+        print("tag : tag_3")
+        print(json.loads(response3.get_data().decode()))
 
 
 if __name__ == '__main__':

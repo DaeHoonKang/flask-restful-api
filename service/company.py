159643abb7e9fa4f37eb1076e904b592865ece59
dhkang <dhkang@nm-monster.com>
2019-04-09 14:18:34 +0900

    [API] 태그 추가/삭제 예외 처리
    Request Body로 들어오는 Json 문자열 파싱에 대해서 예외 처리 추가
    DecodeError, ValueError, 기타 발생 시 모두 400 BadRequest로 처리
    Json 데이터에 필드가 1개라도 없을 경우 400 BadRequest로 처리

--- service/company.py
+++ service/company.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-import re
-import json
 from http import HTTPStatus
 from flask import Blueprint, make_response, jsonify, request
 from db.company import Company
 from exceptions.exception import InvalidParams
+from utils.functions import json_loads
 
 
 company_service = Blueprint('company', __name__, url_prefix='/company')
@@ -12,10 +11,7 @@ company_service = Blueprint('company', __name__, url_prefix='/company')
 
 @company_service.route('/autocomplete', methods=['GET'])
 def get_company_name():
-    """ 회사명 자동완성 검색
-
-    :return:
-    """
+    """ 회사명 자동완성 검색 """
     input = request.args.get('input')
     if not input:
         raise InvalidParams(reason='The input parameter field is required when calling the api')
@@ -25,10 +21,7 @@ def get_company_name():
 
 @company_service.route('/tag', methods=['GET'])
 def get_company_tag():
-    """ 태그로 회사명 검색
-
-    :return:
-    """
+    """ 태그로 회사명 검색 """
     input = request.args.get('input')
     if not input:
         raise InvalidParams(reason='The input parameter field is required when calling the api')
@@ -38,15 +31,12 @@ def get_company_tag():
 
 @company_service.route('/tag', methods=['POST'])
 def put_company_tag():
-    """ 입력한 회사명 정보에 태그 추가
-
-    :return:
-    """
-    data = json.loads(request.data)
+    """ 입력한 회사명 정보에 태그 추가 """
+    data = json_loads(request.data)
     company = data.get('company', None)
     tag = data.get('tag', None)
     if not company or not tag:
-        raise InvalidParams(reason='The parameters field(company, tag) is required when calling the api')
+        raise InvalidParams(reason='One or more fields - company or tag - not found in the json data')
     # update tag
     count = Company.objects(name=company).update(add_to_set__tags=tag)
     return make_response(jsonify({'count': count}), HTTPStatus.OK)
@@ -54,15 +44,12 @@ def put_company_tag():
 
 @company_service.route('/tag', methods=['DELETE'])
 def delete_company_tag():
-    """ 입력한 회사명 정보에 태그 삭제
-
-    :return:
-    """
-    data = json.loads(request.data)
+    """ 입력한 회사명 정보에 태그 삭제 """
+    data = json_loads(request.data)
     company = data.get('company', None)
     tag = data.get('tag', None)
     if not company or not tag:
-        raise InvalidParams(reason='The parameters field(company, tag) is required when calling the api')
+        raise InvalidParams(reason='One or more fields - company or tag - not found in the json data')
     # update tag
     count = Company.objects(name=company).update(pull__tags=tag)
     return make_response(jsonify({'count': count}), HTTPStatus.OK)
